7fe49f5806e0302f24f4090dd8138d00f1a252ab
avnerjose <avnerj.g.r@gmail.com>
2022-10-14 18:11:51 -0300

    Creating text field and book listing on home page

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,6 @@
 import 'package:books_app/controllers/fetchBook.dart';
 import 'package:books_app/models/book.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,32 +10,91 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late List<Book> _books;
-  bool isLoading = true;
+  late List<Book> _books = [];
+  final textEditingController = TextEditingController();
+  bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    getBooks();
+    textEditingController.addListener(_getBooks);
   }
 
-  Future<void> getBooks() async {
-    _books = await fetchBooksBySearch();
-    setState(() {
-      isLoading = false;
-    });
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    ;
+  }
+
+  Future<void> _getBooks() async {
+    if (textEditingController.text.trim() != "") {
+      setState(() {
+        isLoading = true;
+      });
+      var query = textEditingController.text.split(' ').join("+");
+      _books = await fetchBooksBySearch(query);
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: isLoading
-            ? const Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: _books.length,
-                itemBuilder: (context, index) {
-                  return Text(_books[index].title);
-                },
-              ));
+        backgroundColor: const Color.fromRGBO(243, 245, 246, 1),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                decoration:
+                    const BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.4),
+                    blurRadius: 5.0,
+                    spreadRadius: 2.0,
+                    offset: Offset(
+                      2.0,
+                      2.0,
+                    ),
+                  )
+                ]),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Lista de livros",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    TextField(
+                      controller: textEditingController,
+                      decoration: const InputDecoration(
+                          hintText: "Pesquise um livro",
+                          icon: Icon(Icons.search)),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : GridView.count(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16, horizontal: 8),
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                        childAspectRatio: 0.667,
+                        children: List.generate(
+                            _books.length,
+                            (index) => Image.network(
+                                  _books[index].imageUrl,
+                                )),
+                      ),
+              ),
+            ],
+          ),
+        ));
   }
 }
