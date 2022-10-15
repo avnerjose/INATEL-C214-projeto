9081955800e8da593c828f9ca2874d5e9c77b630
avnerjose <avnerj.g.r@gmail.com>
2022-10-15 16:55:11 -0300

    Creating custom colors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/rendering.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF121214);
+  static const Color green = Color(0xFF44BC7F);
+  static const Color dark = Color(0xFF202024);
+  static const Color lightGray = Color(0xFF7C7C8A);
+}
