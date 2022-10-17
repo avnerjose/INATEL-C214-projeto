527e4b208011dd1c4b347822d58d4983797b02f3
avnerjose <avnerj.g.r@gmail.com>
2022-10-17 19:49:43 -0300

    Receiving book from route on book detail page

--- lib/views/book_detail.dart
+++ lib/views/book_detail.dart
@@ -1,51 +1,55 @@
 import 'dart:ui';
-
 import 'package:books_app/core/app_colors.dart';
 import 'package:books_app/models/book.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class BookDetailPage extends StatelessWidget {
-  const BookDetailPage({Key? key}) : super(key: key);
+  const BookDetailPage({super.key, required this.book});
+
+  final Book book;
 
   @override
   Widget build(BuildContext context) {
-    final Book _book = Book(
-        id: "1",
-        authors: ['Jk Rowling'],
-        averageRating: 4,
-        categories: ['Fantasy'],
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque fringilla lorem, nec fermentum lectus rutrum mattis. Etiam placerat, est eu sollicitudin molestie, turpis nibh elementum augue, nec volutpat lacus augue vitae velit. Quisque ac sapien aliquam, gravida turpis vel, tempus ligula. Mauris sed lobortis nisi. Duis dapibus cursus semper. Ut eget aliquet risus, sed interdum diam. Nunc tincidunt est sit amet magna mattis convallis. Praesent sit amet porttitor dui. Aenean interdum neque urna, et pretium ligula tempor quis. Curabitur tincidunt purus a erat hendrerit laoreet.",
-        imageUrl:
-            "http://books.google.com/books/content?id=wmy674GblEUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-        pageCount: 200,
-        publishedDate: "2001",
-        publisher: "Rocco",
-        title: "Harry Potter e o cálice de fogo");
-
     return Scaffold(
         backgroundColor: AppColors.background,
         body: SafeArea(
             child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 150,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(_book.imageUrl),
-                        fit: BoxFit.cover)),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
-                  child: Container(
-                    decoration:
-                        BoxDecoration(color: Colors.black.withOpacity(0.1)),
+              Stack(children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(book.imageUrl),
+                          fit: BoxFit.cover)),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+                    child: Container(
+                      decoration:
+                          BoxDecoration(color: Colors.black.withOpacity(0.1)),
+                    ),
                   ),
                 ),
-              ),
+                Positioned(
+                    top: 8,
+                    left: 8,
+                    child: SizedBox(
+                        height: 38,
+                        width: 38,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            onPrimary: Colors.white,
+                            padding: EdgeInsets.zero,
+                            shape: const CircleBorder(),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(Icons.chevron_left),
+                        )))
+              ]),
               Container(
                 color: AppColors.dark,
                 child: Stack(
@@ -54,30 +58,25 @@ class BookDetailPage extends StatelessWidget {
                     Transform.translate(
                       offset: const Offset(0, -150),
                       child: Image.network(
-                        _book.imageUrl,
+                        book.imageUrl,
                         height: 192,
                         width: 128,
                       ),
                     ),
                     Column(
                       children: [
-                        const SizedBox(height: 120),
+                        const SizedBox(height: 130),
                         Text(
-                          _book.title,
+                          book.title,
                           style: const TextStyle(
                               color: Colors.white,
                               fontSize: 16,
                               fontWeight: FontWeight.w600),
                         ),
                         const SizedBox(height: 8),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ..._book.authors.map((author) => Text(author,
-                                style: const TextStyle(
-                                    color: AppColors.lightGray, fontSize: 14)))
-                          ],
-                        ),
+                        Text(book.authors.join(', '),
+                            style: const TextStyle(
+                                color: AppColors.lightGray, fontSize: 14)),
                         const SizedBox(height: 24),
                         ElevatedButton(
                             onPressed: () {},
@@ -108,7 +107,7 @@ class BookDetailPage extends StatelessWidget {
                                 child: Column(
                                   children: [
                                     Text(
-                                      _book.pageCount.toString(),
+                                      book.pageCount.toString(),
                                       style: const TextStyle(
                                           fontSize: 14,
                                           color: AppColors.lightGray),
@@ -127,7 +126,7 @@ class BookDetailPage extends StatelessWidget {
                                         style: TextStyle(
                                             fontSize: 14,
                                             color: AppColors.lightGray)),
-                                    Text(_book.publishedDate,
+                                    Text(book.publishedDate,
                                         style: const TextStyle(
                                             fontSize: 14,
                                             color: AppColors.lightGray)),
@@ -141,7 +140,7 @@ class BookDetailPage extends StatelessWidget {
                                         style: TextStyle(
                                             fontSize: 14,
                                             color: AppColors.lightGray)),
-                                    Text('${_book.averageRating}/5',
+                                    Text('${book.averageRating}/5.0',
                                         style: const TextStyle(
                                             fontSize: 14,
                                             color: AppColors.lightGray)),
@@ -172,7 +171,7 @@ class BookDetailPage extends StatelessWidget {
                         )),
                     const SizedBox(height: 8),
                     Text(
-                      _book.description,
+                      book.description,
                       style: const TextStyle(color: Colors.white, fontSize: 14),
                     )
                   ],
@@ -194,11 +193,11 @@ class BookDetailPage extends StatelessWidget {
                         )),
                     const SizedBox(height: 8),
                     Text(
-                      '\u2022 Editora: ${_book.publisher}',
+                      '\u2022 Editora: ${book.publisher}',
                       style: const TextStyle(color: Colors.white, fontSize: 14),
                     ),
                     Text(
-                      '\u2022 Categorias: ${_book.categories.join(',')}',
+                      '\u2022 Categorias: ${book.categories.join(',')}',
                       style: const TextStyle(color: Colors.white, fontSize: 14),
                     )
                   ],
