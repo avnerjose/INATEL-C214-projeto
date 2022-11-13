fbf91cede07fabd474a6ab3c4d1a3c30fcc84e56
avnerjose <avnerj.g.r@gmail.com>
2022-11-13 11:38:36 -0300

    Adding purple color to app

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -6,6 +6,7 @@ class AppColors {
   static const Color green = Color(0xFF44BC7F);
   static const Color dark = Color(0xFF202024);
   static const Color lightGray = Color(0xFF7C7C8A);
+  static const Color purple = Color(0xFF996DFF);
 }
 
 MaterialColor createMaterialColor(Color color) {
