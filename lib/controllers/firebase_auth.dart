84be81968a2a2969970fcc6c297fd779a0c6a0c9
avnerjose <avnerj.g.r@gmail.com>
2022-11-13 17:05:12 -0300

    Handling Remember-me option

--- lib/controllers/firebase_auth.dart
+++ lib/controllers/firebase_auth.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseAuthHelper {
-  Future<void> signUp(String name, String email, String password) async {
+  Future<void> signUp(
+      String name, String email, String password, bool rememberMe) async {
     try {
+      await handleRememberMe(rememberMe);
       final credential = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
       User? user = credential.user;
@@ -18,7 +21,32 @@ class FirebaseAuthHelper {
     }
   }
 
+  Future<void> signIn(String email, String password, bool rememberMe) async {
+    try {
+      await handleRememberMe(rememberMe);
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-email' || e.code == 'wrong-password') {
+        throw 'Email ou senha inválidos';
+      } else if (e.code == 'user-not-found' || e.code == 'user-disabled') {
+        throw 'Usuário não encontrado ou desativado';
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  Future<void> handleRememberMe(bool rememberMe) async {
+    final auth = FirebaseAuth.instanceFor(
+        app: Firebase.app(), persistence: Persistence.NONE);
+
+    if (rememberMe) {
+      await auth.setPersistence(Persistence.LOCAL);
+    }
   }
 }
