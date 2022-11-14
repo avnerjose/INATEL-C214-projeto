67e984b0d8992e6fb3356ee7fe6edec611df9a21
avnerjose <avnerj.g.r@gmail.com>
2022-11-13 22:52:55 -0300

    Modifing main to accept auth changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:books_app/core/app_colors.dart';
 import 'package:books_app/views/screens/home/home_screen.dart';
+import 'package:books_app/views/screens/login/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<Widget> getFirstPage() async {
+  return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData && !(snapshot.data!.isAnonymous)) {
+          return const HomeScreen();
+        }
+
+        return const LoginScreen();
+      });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primaryColor: AppColors.dark,
-            primarySwatch: createMaterialColor(AppColors.green)),
-        home: const HomeScreen());
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primaryColor: AppColors.dark,
+          primarySwatch: createMaterialColor(AppColors.green)),
+      home: await getFirstPage()));
 }
