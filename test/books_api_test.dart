b86c4699f4625f4e34f70c71beb6ec2857d8766c
avnerjose <avnerj.g.r@gmail.com>
2022-11-22 09:59:01 -0300

    Deleting mock files

--- test/books_api_test.dart
+++ test/books_api_test.dart
@@ -7,7 +7,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
 import 'books_api_test.mocks.dart';
 
 @GenerateMocks([http.Client])
--- test/firebase_auth_test.dart
+++ test/firebase_auth_test.dart
@@ -6,7 +6,6 @@ import 'package:books_app/shared/exceptions/weak_password.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
-import 'package:mockito/mockito.dart';
 
 void main() {
   final mockUser = MockUser(
